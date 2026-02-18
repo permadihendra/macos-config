675f29c82991d9d1314feb4b106a49ba6fb26367
permadihendra <hendrapermadi69@gmail.com>
2026-02-18 13:23:50 +0700

    update keyboard press tweak, because too fast

--- optimize/keyboard.sh
+++ optimize/keyboard.sh
@@ -4,8 +4,8 @@ set -e
 case "$1" in
   enable)
     # Slightliy faster than default (~30%)
-    defaults write NSGlobalDomain KeyRepeat -int 4
-    defaults write NSGlobalDomain InitialKeyRepeat -int 18
+    defaults write NSGlobalDomain KeyRepeat -int 5
+    defaults write NSGlobalDomain InitialKeyRepeat -int 16
     defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
     ;;
   restore)
